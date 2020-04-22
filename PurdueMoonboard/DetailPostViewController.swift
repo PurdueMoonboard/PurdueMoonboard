ec595e116fc9e286cc5548cf05b69e232cc16595
S-Tarr <tarrs@purdue.edu>
2020-04-22 19:41:40 -0400

    linking detailpost with search results

--- PurdueMoonboard/DetailPostViewController.swift
+++ PurdueMoonboard/DetailPostViewController.swift
@@ -1,8 +1,8 @@
 //
-//  MainFeedViewController.swift
+//  DetailPostViewController.swift
 //  PurdueMoonboard
 //
-//  Created by Sam on 4/5/20.
+//  Created by Sam on 4/22/20.
 //  Copyright © 2020 tarrGames(). All rights reserved.
 //
 
@@ -11,16 +11,14 @@ import Parse
 import AlamofireImage
 import MessageInputBar
 
-class MainFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MessageInputBarDelegate {
-    
-    @IBOutlet weak var tableView: UITableView!
+class DetailPostViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MessageInputBarDelegate  {
     
     var showsCommentBar = false
     let commentBar = MessageInputBar()
-    var posts = [PFObject]()
     
-    var selectedPost: PFObject!
-    
+    var post: PFObject?
+
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,49 +28,20 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
         
         tableView.delegate = self
         tableView.dataSource = self
-
+        // Do any additional setup after loading the view.
+        
         tableView.keyboardDismissMode = .interactive
         let center = NotificationCenter.default
         center.addObserver(self, selector: #selector(keyboardWillBeHidden(note:)), name: UIResponder.keyboardWillHideNotification, object:  nil)
-        self.hideKeyboardWhenTappedAround()
-    }
-    //MARK: Keyboard functions
-    func hideKeyboardWhenTappedAround() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-    @IBAction func textFieldShouldReturn(_ sender: UITextField) {
-        dismissKeyboard()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear called")
-        let query = PFQuery(className: "Posts")
-        query.includeKeys(["author", "comments", "comments.author", "VGrade", "route_name"])
-        query.limit = 20
-        
-        query.findObjectsInBackground { (posts, error) in
-            if posts != nil {
-                self.posts = posts!
-                self.posts = self.posts.reversed()
-                self.tableView.reloadData()
-            }
-        }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let post = posts[section]
-        let comments = (post["comments"] as? [PFObject]) ?? []
+        let post = self.post
+        let comments = (post?["comments"] as? [PFObject]) ?? []
         
         return comments.count + 2
     }
     func numberOfSections(in tableView: UITableView) -> Int {
-        return posts.count
+        return 1
     }
     override var inputAccessoryView: UIView? {
         return commentBar
@@ -82,22 +51,22 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let post = posts[indexPath.section]
-        let comments = (post["comments"] as? [PFObject]) ?? []
+        let post = self.post
+        let comments = (post?["comments"] as? [PFObject]) ?? []
         
         
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostTableViewCell
             
-            let user = post["author"] as! PFUser
+            let user = post?["author"] as! PFUser
             cell.usernameLabel.text = user.username
-            cell.vGrade.text = post["VGrade"] as? String
+            cell.vGrade.text = post?["VGrade"] as? String
             cell.vGrade.textColor = findVGradeColor(vGrade: cell.vGrade.text ?? "V0")
-            let routeName = post["route_name"] as? String
+            let routeName = post?["route_name"] as? String
             cell.routeName.text = "Route: " + (routeName ?? "No Name")
-            cell.commentLabel.text = post["caption"] as? String
+            cell.commentLabel.text = post?["caption"] as? String
             
-            let imageFile = post["image"] as! PFFileObject
+            let imageFile = post?["image"] as! PFFileObject
             let urlString = imageFile.url!
             let url = URL(string: urlString)!
             
@@ -121,30 +90,6 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
             return cell
         }
     }
-    
-    
-    @IBAction func onLogout(_ sender: Any) {
-        PFUser.logOut()
-        let main = UIStoryboard(name: "Main", bundle: nil)
-        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
-        let sceneDelegate = self.view.window?.windowScene?.delegate as! SceneDelegate
-        sceneDelegate.window?.rootViewController = loginViewController
-    }
-    
-    
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let post = posts[indexPath.section]
-        let comments = (post["comments"] as? [PFObject]) ?? []
-        
-        if indexPath.row == comments.count + 1 {
-            showsCommentBar = true
-            becomeFirstResponder()
-            commentBar.inputTextView.becomeFirstResponder()
-        }
-        selectedPost = post
-    }
-    
     @objc func keyboardWillBeHidden(note: Notification) {
         commentBar.inputTextView.text = nil
         showsCommentBar = false
@@ -155,12 +100,12 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
         
         let comment = PFObject(className: "Comments")
         comment["text"] = text
-        comment["post"] = selectedPost
+        comment["post"] = post
         comment["author"] = PFUser.current()!
 
-        selectedPost.add(comment, forKey: "comments")
+        post?.add(comment, forKey: "comments")
         
-        selectedPost.saveInBackground { (succes, error) in
+        post?.saveInBackground { (succes, error) in
             if succes {
                 print("Comment was saved \(String(describing: comment["text"]))")
             } else {
@@ -195,6 +140,7 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
             return .orange
         }
     }
+
     /*
     // MARK: - Navigation
 
